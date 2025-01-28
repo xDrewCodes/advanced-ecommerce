4215fe308ba7abe7aaeef1402e604b6e5c12fdb7
Drew Allred <drew.e.allred@gmail.com>
2025-01-28 14:26:22 -0500

    Re-designed home page, fixed themes.

--- books.js
+++ books.js
@@ -1,4 +1,38 @@
 
+function renderBooks() {
+
+  const booksWrapper = document.querySelector('.books')
+  const books = getBooks()
+
+  const bookElements = books.map((book) => {
+
+    return `<div class="book">
+    <figure class="book__img--wrapper">
+      <img class="book__img" src="${book.url}" alt="">
+    </figure>
+    <div class="book__title">
+      ${book.title}
+    </div>
+    <div class="book__ratings">
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star-half-alt"></i>
+    </div>
+    <div class="book__price">
+      <span class="book__price--normal">$${book.originalPrice.toFixed(2)}</span> $00
+    </div>
+  </div>`
+  })
+
+  booksWrapper.innerHTML = bookElements.join('')
+  console.log(bookElements)
+
+}
+
+renderBooks()
+
 
 // FAKE DATA
 function getBooks() {
@@ -6,7 +40,7 @@ function getBooks() {
     {
       id: 1,
       title: "Crack the Coding Interview",
-                url: "assets/crack the coding interview.png",
+      url: "assets/crack the coding interview.png",
       originalPrice: 49.95,
       salePrice: 14.95,
       rating: 4.5,
